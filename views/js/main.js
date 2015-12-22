6145fa592c9676a3a7f333bc8bf5d3acb3494ba0
argbx <overandover@gmail.com>
2015-12-22 15:50:21 +0100

    addeventlistener update

--- views/js/main.js
+++ views/js/main.js
@@ -587,6 +587,9 @@ document.addEventListener('DOMContentLoaded', function() {
     // declaring elem outside the loop to prevent it from being created during the loop levent 20151221
     var elem;
     var s = 256;
+    // created movvingpizzas var taking it out of the loop
+    var movingpizzas = document.getElementById('movingPizzas1');
+    
     for (var i = 0; i < total; i++) {
         elem = document.createElement('img');
         elem.className = 'mover';
@@ -595,7 +598,7 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.style.width = '73.333px';
         elem.basicLeft = (i % cols) * s;
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
-        document.getElementById('movingPizzas1').appendChild(elem);
+        movingpizzas.appendChild(elem);
     }
     updatePositions();
 });
